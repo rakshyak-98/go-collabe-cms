e0637a72deab1042584bfafa20c0bf9a194fbb74
rakshyak-98 <rakshak1998@gmail.com>
2024-08-22 19:55:39 +0530

    format

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type user struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-
 
 var person = []user{
 	{ID: "1", Name: "Rakshya", Email: "rakshyak@gmail.com", Password: "123456"},
@@ -19,13 +19,24 @@ var person = []user{
 	{ID: "3", Name: "Rakshya", Email: "rakshyak@gmail.com", Password: "123456"},
 }
 
-
-func main(){
+func main() {
 	request := gin.Default()
 	request.GET("/users", getUsers)
+	request.GET("/users/:id", getUserById)
 	request.Run("localhost:8000")
 }
 
-func getUsers(context *gin.Context){
+func getUsers(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, person)
+}
+
+func getUserById(context *gin.Context) {
+	id := context.Param("id")
+
+	for _, a := range person {
+		if a.ID == id {
+			context.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
 }
