de12f90a7ca2775bc90735d149ee00547aa826bc
rakshyak-98 <rakshak1998@gmail.com>
2024-09-22 22:40:53 +0530

    leet code solution using go

---
+++ DSA/array/subarray.go
@@ -0,0 +1,26 @@
+package array
+
+import "fmt"
+
+/*
+Q: https://leetcode.com/problems/largest-subarray-length-k/
+*/
+
+func main() {
+	arr := []int{1, 3, 2, 6, -1, 4, 1, 8, 2}
+	k := 5
+	result := make([]float64, 9)
+
+	windowSum := 0
+	windowStart := 0
+
+	for windowEnd := 0; windowEnd < len(arr); windowEnd += 1 {
+		windowSum += arr[windowEnd]
+		if windowEnd >= k-1 {
+			result = append(result, float64(windowSum)/float64(k))
+			windowSum -= arr[windowStart]
+			windowStart += 1
+		}
+	}
+	fmt.Println(result)
+}
